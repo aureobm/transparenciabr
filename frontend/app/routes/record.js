8cd4170ce4b5ac2642d0de98768223b8e7d7d83a
Aureo Bezerra Modolo <aureo.modolo@accenture.com>
2020-04-27 16:36:06 -0300

    Adicionado parametro pagina a rota

--- frontend/app/routes/record.js
+++ frontend/app/routes/record.js
@@ -1,8 +1,18 @@
 import Route from '@ember/routing/route';
 
 export default class RecordRoute extends Route {
-  async model() {
-    let response = await fetch('http://www.transparencia.gov.br/api-de-dados/emendas?pagina=1');
+  queryParams = {
+    pagina: {
+      refreshModel: true
+    }
+  };
+  
+  async model(params) {
+	let pagina = 1;
+	if(params.pagina) {
+		pagina = params.pagina;
+	}
+    let response = await fetch('http://www.transparencia.gov.br/api-de-dados/emendas?pagina='+pagina);
     let parsed = await response.json();
 
     return parsed;
